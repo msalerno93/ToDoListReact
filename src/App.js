512121c50edeab93edc80570705a6e154551539a
Michael Salerno <msalerno1993@gmail.com>
2022-08-03 18:49:03 -0400

    finalizing functionality of to do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [list, setList] = useState([])
+  const [input, setInput] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="Header">To Do List</div>
+      <input 
+        type="text" 
+        className="input"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+       />
+       <button>Add Task</button>
     </div>
   );
 }
